165bb8b583c44afde713cd71082bb12ec42b5d47
jessex <essexjosh@gmail.com>
2012-01-19 21:59:46 -0500

    Removing superfluous return statements that I somehow missed. What is WRONG with me

--- ruby/0072_find_maximum_subarray.rb
+++ ruby/0072_find_maximum_subarray.rb
@@ -18,11 +18,11 @@ def find_maximum_subarray(a, low, high)
       find_max_crossing_subarray a, low, mid, high
 
     if left_sum >= right_sum and left_sum >= cross_sum
-      return [left_low, left_high, left_sum]
+      [left_low, left_high, left_sum]
     elsif right_sum >= left_sum and right_sum >= cross_sum
-      return [right_low, right_high, right_sum]
+      [right_low, right_high, right_sum]
     else
-      return [cross_low, cross_high, cross_sum]
+      [cross_low, cross_high, cross_sum]
     end
   end
 end
